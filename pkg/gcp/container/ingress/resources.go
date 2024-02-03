d906285a004afd5ba063354279a141402c6480f1
Swarup Donepudi <swarup.donepudi@gmail.com>
2024-02-02 19:45:03 -0800

    [skip ci] fixed nil pointer error with ingress

--- pkg/gcp/container/ingress/resources.go
+++ pkg/gcp/container/ingress/resources.go
@@ -7,6 +7,7 @@ import (
 	"github.com/plantoncloud-inc/kube-cluster-pulumi-blueprint/pkg/gcp/container/ingress/gateway"
 	"github.com/plantoncloud-inc/kube-cluster-pulumi-blueprint/pkg/gcp/container/ingress/service"
 	"github.com/plantoncloud-inc/kube-cluster-pulumi-blueprint/pkg/gcp/network/ip"
+	"github.com/plantoncloud/planton-cloud-apis/zzgo/cloud/planton/apis/v1/code2cloud/deploy/kubecluster/stack/gcp"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
@@ -15,9 +16,14 @@ type Input struct {
 	AddedIpAddresses       *ip.AddedIngressIpAddresses
 	AddedContainerClusters *cluster.AddedContainerClusterResources
 	AddedAddonResources    *addon.AddedResources
+	IstioAddonInput        *gcp.AddonsIstio
 }
 
 func Resources(ctx *pulumi.Context, input *Input) error {
+	if input.IstioAddonInput == nil || !input.IstioAddonInput.Enabled {
+		return nil
+	}
+
 	//Resources adds kubernetes service resources that create internal and external load balancers
 	//adds a kafka gateway that opens up port 9092 and does a tls passthrough
 	if err := service.Resources(ctx, &service.Input{
